df69bfc183f325ae518e0d9e64c0ea5db7c6a976
yutabee <bsk87m@gmail.com>
2022-08-11 16:38:04 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useState } from 'react';
 import './App.css';
+import UserCard from './components/UserCard';
+import { User } from './types/api/user';
+import { UserProfile } from './types/userProfile';
 
 function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onClickFetchUser = () => {
+    setLoading(true);
+
+    axios.get<Array<User>>('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}${user.username}`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
+      }).catch(() => {
+        setError(true);
+
+      }).finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClickFetchUser}>データを取得</button>
+      {error ? (
+        <p style={{ color:'red' }}>データの取得に失敗しました</p>
+      ) : loading ? (
+      <p>Loading...</p>
+      ):(
+            userProfiles?.map((user) => (
+          <UserCard key={user.id} user={user} />
+      )      
+        ))
+      }
     </div>
   );
 }
---
+++ src/components/UserCard.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react'
+import { UserProfile } from '../types/userProfile';
+
+type Props = {
+    user: UserProfile;
+};
+
+
+const UserCard: FC<Props> = (props) => {
+    const { user } = props;
+    
+    
+
+  return (
+      <div style={{ 
+        border: 'solid 1px #ccc',
+        borderRadius: '8px',
+        padding: '0 16px',
+        margin: '8px',
+      }}>
+          
+        <dl>
+            <dt>名前</dt>
+            <dd>{user.name}</dd>
+            <dt>メール</dt>
+            <dd>{user.email}</dd> 
+            <dt>住所</dt>
+            <dd>{user.address}</dd>
+        </dl>
+    </div>
+  )
+}
+
+export default UserCard
